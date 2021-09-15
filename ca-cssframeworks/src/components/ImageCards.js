cfe81ff69e773891ae0c3d1042c8b155487c8866
Thomas Kildahl <69842791+ThomasK97@users.noreply.github.com>
2021-09-16 00:18:44 +0200

    updated imagecards

--- ca-cssframeworks/src/components/ImageCards.js
+++ ca-cssframeworks/src/components/ImageCards.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar, Form, FormControl, Button, Nav, NavDropdown, Carousel, Tabs, Tab, Container, Row, Col, Accordion, Card } from 'react-bootstrap';
+import { Navbar, Form, FormControl, Button, Nav, NavDropdown, Carousel, Tabs, Tab, Container, Row, Col, Accordion, Card, CardGroup} from 'react-bootstrap';
 import image1 from './../News images/news-1.jpg';
 import image2 from './../News images/news-2.jpg';
 import image3 from './../News images/news-3.jpg';
@@ -36,39 +36,64 @@ function ImageCards() {
       </Nav.Item>
     </Nav>
   </Card.Header>
+  <CardGroup>
+  <Card style={{ width: '8rem' }}>
+    <Card.Img variant="top" src={image1} />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This content is a little bit longer.
+      </Card.Text>
+    </Card.Body>
+    <br></br>
+      <Button variant="primary">More</Button>
+    
+  </Card>
   <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src={image1} />
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Button variant="primary">More</Button>
-  </Card.Body>
-</Card>
-<Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src={image2} />
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Button variant="primary">More</Button>
-  </Card.Body>
-</Card>
-<Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src={image3} />
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Button variant="primary">More</Button>
-  </Card.Body>
-</Card>
+    <Card.Img variant="top" src={image2} />
+
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This card has supporting text below as a natural lead-in to additional
+        content.
+      </Card.Text>
+    </Card.Body>
+    <br></br>
+      <Button variant="primary">More</Button>
+    
+  </Card>
+  <Card>
+    <Card.Img variant="top" src={image3} />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This card has even longer content than the first to
+        show that equal height action.
+      </Card.Text>
+      
+    </Card.Body>
+    <br></br>
+      <Button variant="primary">More</Button>
+    
+  </Card>
+  <Card style={{ width: '8rem' }}>
+    <Card.Img variant="top" src={image7} />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This content is a little bit longer.
+      </Card.Text>
+    </Card.Body>
+    <br></br>
+      <Button variant="primary">More</Button>
+    
+  </Card>
+</CardGroup>
+
   
 </Card>
             
