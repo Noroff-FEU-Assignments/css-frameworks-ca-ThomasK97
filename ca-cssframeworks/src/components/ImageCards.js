df37ed0c0138a06a588484ec045557708d627627
Thomas Kildahl <69842791+ThomasK97@users.noreply.github.com>
2021-09-20 16:25:46 +0200

    Update ImageCards.js

--- ca-cssframeworks/src/components/ImageCards.js
+++ ca-cssframeworks/src/components/ImageCards.js
@@ -127,6 +127,14 @@ function ImageCards() {
   
 </Container>
 
+<ButtonToolbar aria-label="Toolbar with button groups">
+  <ButtonGroup className="me-2" aria-label="First group">
+    <Button>1</Button> <Button>2</Button> <Button>3</Button> <Button>4</Button>
+  </ButtonGroup>
+
+ 
+</ButtonToolbar>
+
 
 
   
